846fdf3f3bddfe0a8f39bb2086ce312f9bf5a4dd
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-27 15:52:25 -0300

    test: ensure singup controller return an error if body not provided email

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -18,4 +18,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_p@assw0rd',
+        passwordConfirmation: 'any_p@assw0rd'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })

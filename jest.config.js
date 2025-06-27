829b35449fb25d3a29ce2536be613ccf75bec5aa
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-27 14:52:59 -0300

    fix: dependencies

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,20 @@
+import { SignUpController } from './singup';
+
+describe('SignUp Controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        // SUT - SYSTEM UNDER TEST
+        const sut = new SignUpController();
+        
+        const httpRequest = {
+            body: {
+                email: "any@email",
+                password: "any_p@assw0rd",
+                passwordConfirmation: "any_p@assw0rd",
+            }
+        };
+
+        const httpResponse = sut.handle(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(40)
+    });
+});

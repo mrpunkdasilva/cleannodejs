6829d68b1e7eac4962138969d5f337ec8db9485e
Mr Punk da Silva <gustavojs417@gmail.com>
2025-06-27 15:17:27 -0300

    teste

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,20 +1,20 @@
-import { SignUpController } from './signup';
+import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
-    test('Should return 400 if no name is provided', () => {
-        // SUT - SYSTEM UNDER TEST
-        const sut = new SignUpController();
-        
-        const httpRequest = {
-            body: {
-                email: "any@email",
-                password: "any_p@assw0rd",
-                passwordConfirmation: "any_p@assw0rd",
-            }
-        };
+  test('Should return 400 if no name is provided', () => {
+    // SUT - SYSTEM UNDER TEST
+    const sut = new SignUpController()
 
-        const httpResponse = sut.handle(httpRequest);
+    const httpRequest = {
+      body: {
+        email: 'any@email',
+        password: 'any_p@assw0rd',
+        passwordConfirmation: 'any_p@assw0rd'
+      }
+    }
 
-        expect(httpResponse.statusCode).toBe(400)
-    });
-});
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
